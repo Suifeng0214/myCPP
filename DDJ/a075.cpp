54f2a265b6b9c04c52c0cdf8ecbaff7ef992f18b
Suifeng0214 <47234216+Suifeng0214@users.noreply.github.com>
2021-05-25 17:58:18 +0800

    Update a075.cpp

--- DDJ/a075.cpp
+++ DDJ/a075.cpp
@@ -6,41 +6,36 @@ using namespace std;
 #define Y second
 #define lowbit(x) (x&-x)
 #define MP(x, y) make_pair(x, y)
-int a[5000000];
+int a[4800000];
+int now, l, r;
 signed main() 
 { 
 //	freopen("in.txt", "w", stdout);
 	ios::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
 	int n, k, cnt=0;
-	while(cin >> n >> k){
-		for (int i = 0; i < n; i++){
-			cin >> a[i];
-		}
-		int now=0, l=0, r=0;
-		while(cnt < n){
-			if (r>=n) return 0;
-			now += a[r];
-		//	cout << now << endl;
-			if (now == k){
-				cout << r+1 << " \n"[++cnt==n];
-				if (l>=n) return 0;
+	cin >> n >> k;
+	for (int i = 0; i < n; i++){
+		cin >> a[i];
+	}
+	now=0, l=0, r=0, cnt=0;
+	while(cnt < n){
+		now += a[r];
+		if (now == k){
+			cout << r+1 << " \n"[++cnt==n];
+			now -= a[l];
+			l++;
+		}else if (now > k){
+			while(now > k){
+				cout << r << " \n"[++cnt==n];
 				now -= a[l];
 				l++;
-			}else if (now > k){
-				while(now > k){
-					cout << r << " \n"[++cnt==n];
-					if (l>=n) return 0;
-					now -= a[l];
-				//	cout << now << "@" << endl;
-					l++;
-				}
 			}
-			r++;
-			if (r > n-1){
-				while(cnt < n){
-					cout << n << " \n"[++cnt==n];
-				}
+		}
+		r++;
+		if (r > n-1){
+			while(cnt < n){
+				cout << n << " \n"[++cnt==n];
 			}
 		}
 	}
